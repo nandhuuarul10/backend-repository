31e3088a28f6d15592d6007969547221c5150bfb
nandhuuarul10 <nandhuarul10@gmail.com>
2026-07-07 15:02:14 +0530

    Update CorsConfig.java

--- src/main/java/com/example/Eventsphere/security/CorsConfig.java
+++ src/main/java/com/example/Eventsphere/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
+        config.setAllowedOrigins(List.of("*"
             "http://localhost:5500",
             "http://127.0.0.1:5500",
             "http://localhost:5501",
